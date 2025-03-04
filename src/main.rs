e0c93f6f1bcef8628e9bcb137eff2c9cb56b7c03
HasChad <troll.yahya@gmail.com>
2025-03-04 05:21:44 +0300

    - added big flower

--- src/main.rs
+++ src/main.rs
@@ -20,72 +20,125 @@ fn view(app: &App, _model: &Model, frame: Frame) {
 
     let window_width = app.window_rect().w();
 
-    let white_flower_pos = window_width / -2. + 150.;
-    let purple_flower_pos = window_width / 2. - 150.;
+    let white_flower_pos = window_width / -2. + 200.;
+    let purple_flower_pos = window_width / 2. - 200.;
+    let big_flower_pos = 0.;
 
-    let speed = 3000. / window_width;
+    let mut speed = 3000. / window_width;
+
+    speed = ease_in_sine(speed);
+    println!("speed = {}", speed);
     let spread = 2.0;
 
-    // ! white flower
-    let points = (0..50).map(|i| {
-        let x = i as f32;
-        let point = pt2(
-            white_flower_pos
-                + map_range(
-                    (app.time * speed + x / spread).sin(),
-                    -1.0,
-                    1.0,
-                    -25.0,
-                    25.0,
-                ),
-            x * -10.0,
-        );
-        (point, GREEN)
-    });
-    draw.polyline().weight(10.).points_colored(points);
+    if window_width > 400.0 {
+        // ! white flower
+        let points = (0..50).map(|i| {
+            let x = i as f32;
+            let point = pt2(
+                white_flower_pos
+                    + map_range(
+                        (app.time * speed + x / spread).sin(),
+                        -1.0,
+                        1.0,
+                        -25.0,
+                        25.0,
+                    ),
+                x * -10.0,
+            );
+            (point, GREEN)
+        });
+        draw.polyline().weight(10.).points_colored(points);
 
-    for degree in (0..=360).step_by(30) {
-        let radus = deg_to_rad(app.time * speed * 10.0 + degree as f32);
-        let x = radus.sin() * 75.;
-        let y = radus.cos() * 75.;
+        for degree in (0..=360).step_by(30) {
+            let radus = deg_to_rad(app.time * speed * 10.0 + degree as f32);
+            let x = radus.sin() * 75.;
+            let y = radus.cos() * 75.;
+            draw.ellipse()
+                .color(WHITE)
+                .w_h(50., 150.)
+                .x_y(white_flower_pos + x, y)
+                .rotate(-radus);
+        }
+        draw.ellipse().x_y(white_flower_pos, 0.0).color(LIGHTBLUE);
+
+        // ! purple flower
+        let points = (0..50).map(|i| {
+            let x = i as f32;
+            let point = pt2(
+                purple_flower_pos
+                    + map_range(
+                        -(app.time * speed + x / spread).sin(),
+                        -1.0,
+                        1.0,
+                        -25.0,
+                        25.0,
+                    ),
+                x * -10.0,
+            );
+            (point, GREEN)
+        });
+        draw.polyline().weight(15.).points_colored(points);
+
+        for degree in (0..=360).step_by(45) {
+            let radus = deg_to_rad(app.time * speed * -10.0 + degree as f32);
+            let x = radus.sin() * 75.;
+            let y = radus.cos() * 75.;
+            draw.ellipse()
+                .color(PURPLE)
+                .w_h(100., 100.)
+                .x_y(purple_flower_pos + x, y)
+                .rotate(-radus);
+        }
+        draw.ellipse().x_y(purple_flower_pos, 0.0).color(WHITE);
+    } else {
+        let points = (0..50).map(|i| {
+            let x = i as f32;
+            let point = pt2(
+                big_flower_pos
+                    + map_range(
+                        -(app.time * speed + x / spread).sin(),
+                        -1.0,
+                        1.0,
+                        -25.0,
+                        25.0,
+                    ),
+                x * -10.0,
+            );
+            (point, hsv(x / 50.0, 1.0, 1.0))
+        });
+        draw.polyline().weight(15.).points_colored(points);
+
+        for degree in (0..=360).step_by(45) {
+            let radus = deg_to_rad(app.time * speed * 10.0 + degree as f32);
+            let x = radus.sin() * 75.;
+            let y = radus.cos() * 75.;
+            draw.ellipse()
+                .color(WHITE)
+                .w_h(75., 150.)
+                .x_y(big_flower_pos + x, y)
+                .rotate(-radus);
+        }
+
+        for degree in (0..=360).step_by(45) {
+            let radus = deg_to_rad(app.time * speed * -10.0 + degree as f32);
+            let x = radus.sin() * 75.;
+            let y = radus.cos() * 75.;
+            draw.ellipse()
+                .color(PURPLE)
+                .w_h(50., 50.)
+                .x_y(big_flower_pos + x, y)
+                .rotate(-radus);
+        }
         draw.ellipse()
-            .color(WHITE)
-            .w_h(50., 150.)
-            .x_y(white_flower_pos + x, y)
-            .rotate(-radus);
+            .w_h(50., 50.)
+            .x(big_flower_pos)
+            .color(YELLOWGREEN);
     }
-    draw.ellipse().x_y(white_flower_pos, 0.0).color(LIGHTBLUE);
-
-    // ! purple flower
-    let points = (0..50).map(|i| {
-        let x = i as f32;
-        let point = pt2(
-            purple_flower_pos
-                + map_range(
-                    -(app.time * speed + x / spread).sin(),
-                    -1.0,
-                    1.0,
-                    -25.0,
-                    25.0,
-                ),
-            x * -10.0,
-        );
-        (point, GREEN)
-    });
-    draw.polyline().weight(15.).points_colored(points);
-
-    for degree in (0..=360).step_by(45) {
-        let radus = deg_to_rad(app.time * speed * -10.0 + degree as f32);
-        let x = radus.sin() * 75.;
-        let y = radus.cos() * 75.;
-        draw.ellipse()
-            .color(PURPLE)
-            .w_h(100., 100.)
-            .x_y(purple_flower_pos + x, y)
-            .rotate(-radus);
-    }
-    draw.ellipse().x_y(purple_flower_pos, 0.0).color(WHITE);
 
     draw.background().color(BLACK);
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn ease_in_sine(x: f32) -> f32 {
+    1.0 - ((x * PI) / 2.0).cos()
 }
